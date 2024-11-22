54bd4951690fe142a2e65ecff200ca16816814ed
I461-TKI <lukankinv20@gmail.com>
2024-11-22 14:44:05 +0300

    main подправил

--- CircularLinkedListEX/main.cpp
+++ CircularLinkedListEX/main.cpp
@@ -1,23 +1,50 @@
-#include "..\Solver\Node.h"
-#include "..\Solver\CircularLinkedList.h"
 #include <iostream>
+#include <string>
+#include "..\Solver\CircularLinkedList.h" 
 
 int main() {
-    CircularLinkedList<int> list;
+    rut::miit::llist::CircularLinkedList<int> list;
 
-    list.add(1);
-    list.add(2);
-    list.add(3);
-    list.add(4);
+    std::cout << "Добавляем элементы 10, 20, 30 в список..." << std::endl;
+    list.Add(10);
+    list.Add(20);
+    list.Add(30);
 
-    std::cout << "Circular Linked List contents: ";
-    list.display();
+    
+    std::wcout << L"Текущий список: " << list.ToString(list) << std::endl;
 
-    std::cout << "Removing the front element..." << std::endl;
-    list.removeFront();
+    std::cout << "Вставляем элемент 15 на позицию 1..." << std::endl;
+    list.InsertAt(15, 1);
+    std::wcout << L"Список после вставки: " << list.ToString(list) << std::endl;
 
-    std::cout << "Updated list: ";
-    list.display();
+    
+    std::cout << "Удаляем элемент с позиции 2..." << std::endl;
+    list.RemoveAt(2);
+    std::wcout << L"Список после удаления: " << list.ToString(list) << std::endl;
+
+    
+    int searchValue = 20;
+    std::cout << "Ищем элемент " << searchValue << " в списке..." << std::endl;
+    bool found = list.Find(searchValue);
+    if (found) {
+        std::cout << "Элемент " << searchValue << " найден." << std::endl;
+    }
+    else {
+        std::cout << "Элемент " << searchValue << " не найден." << std::endl;
+    }
+
+    
+    std::cout << "Удаляем первый элемент списка..." << std::endl;
+    list.RemoveFront();
+    std::wcout << L"Список после удаления первого элемента: " << list.ToString(list) << std::endl;
+
+    
+    if (list.IsEmpty()) {
+        std::cout << "Список пуст." << std::endl;
+    }
+    else {
+        std::cout << "Список не пуст." << std::endl;
+    }
 
     return 0;
 }
